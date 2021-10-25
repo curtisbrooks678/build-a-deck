eca1467c2783888c9c5346afeaf37580e043d870
Curtis Brooks <curtisbrooks678@gmail.com>
2021-10-25 14:15:19 -0700

    completed exercise.
    Co-authored-by: Curtis Brooks <curtisbrooks678@gmail.com>
    Co-Authored-by: Jacob Wilson <jacobleeeugenewilson@gmail.com>

--- Desktop/Epicodus/build-a-deck/js/scripts.js
+++ Desktop/Epicodus/build-a-deck/js/scripts.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
-  let iceCreamArr = ["Chocolate", "Mint", "Vanilla", "Strawberry"]
-  let index = 0;
-  iceCreamArr.forEach(function(element) {
-    $("ul#ice-cream-list").append("<li>" + iceCreamArr[index] + "</li>");
-    index += 1;
+
+  const ranks = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"];
+  const suits = ["Clubs", "Diamonds", "Spades", "Hearts"];
+  let cardDeckArr = []
+  let suitIndex = 0;
+
+  suits.forEach(function(suit) {
+    let rankIndex = 0;
+    ranks.forEach(function(rank){
+      $("ul#card-list").append("<li>" + ranks[rankIndex] + " of " + suits[suitIndex] + "</li>");
+      rankIndex += 1;
+    });
+    suitIndex +=1;
   });
 });
-
